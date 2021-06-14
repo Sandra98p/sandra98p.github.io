fd6c3efca13ef4a2ca1822620f6e02023a80b9fe
Sandra <64126420+Sandra98p@users.noreply.github.com>
2021-06-14 22:55:49 +0200

    Add files via upload

---
+++ Website/javascript.js
@@ -0,0 +1,15 @@
+function on() {
+    document.getElementById("pop-up-mobile").style.display = "block";
+}
+  
+function off() {
+    document.getElementById("pop-up-mobile").style.display = "none";
+}
+
+function an() {
+  document.getElementById("pop-up-desktop").style.display = "block";
+}
+
+function aus() {
+  document.getElementById("pop-up-desktop").style.display = "none";
+}
